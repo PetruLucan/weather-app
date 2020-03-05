c7d793aa9e2b5678a696759cc49fa504bb8974ba
PetruLucan <lucanpetru11@gmail.com>
2020-03-05 21:07:16 +0200

    js vanilla based weather app

---
+++ main.js
@@ -0,0 +1,151 @@
+let appId = 'f91c342855202675de5f13d23f2a977c';
+let units = 'metric';
+let searchMethod;
+// search by city/ZIP code/ ID
+
+function getSearchMethod(searchTerm) {
+    if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else
+        searchMethod = 'q'
+}
+
+document.getElementById('city-idResultsBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if (searchTerm)
+        searchWeather(searchTerm);
+    
+    cityIdZip.classList.add('hide-show');
+    document.getElementById('displayResults').classList.remove('hide-show');
+    document.getElementById('displayDate').innerHTML = 'On date: ' + dateInput.value;
+
+    
+
+})
+
+
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(resultFromServer) {
+    let status = document.getElementById('responseStatus');
+    if(resultFromServer.cod === 200){
+        status.innerHTML = 'Request processed with succes !!'
+    }else if(resultFromServer.cod !== 200){
+        status.style.color = '#f92b2b'
+        status.textContent = 'Failed to process this request' + ' - ' + resultFromServer.message +'!!';
+    }
+    console.log(resultFromServer);
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = "url('img/clear.jpg')";
+            break;
+        case 'Clouds':
+            document.body.style.backgroundImage = "url('img/cloudy.jpg')";
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage = "url('img/rain.jpg')";
+            break;
+
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('img/storm.jpg')";
+            break;
+
+        case 'Snow':
+            document.body.style.backgroundImage = "url('img/snow.jpg')";
+            break;
+
+        default:
+            document.body.style.backgroundImage = "url('img/default.jpg')";
+            break;
+    }
+
+    let cityNameHeader = document.getElementById('cityNameHeader');
+    let windSpeed = document.getElementById('windSpeed');
+    let currentConditionIcon = document.getElementById('currentConditionIcon');
+    let currentConditionDescription = document.getElementById('currentConditionDescription');
+    let temperature = document.getElementById('temperature');
+    let humidity = document.getElementById('humidity');
+
+    currentConditionIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+    let resultDescription = resultFromServer.weather[0].description;
+    currentConditionDescription.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+
+    temperature.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+    windSpeed.innerHTML = Math.floor(resultFromServer.wind.speed) + ' m/s';
+    cityNameHeader.innerHTML = resultFromServer.name;
+    humidity.innerHTML = resultFromServer.main.humidity + '%';
+    
+    
+}
+
+//search by coordinates
+
+document.getElementById('coordsResultBtn').addEventListener('click', () => {
+    let latInput = document.getElementById('latitude').value;
+    let longInput = document.getElementById('longitude').value;
+    searchByCoords(latInput, longInput);
+
+    coordsPage.classList.add('hide-show');
+    document.getElementById('displayResults').classList.remove('hide-show');
+    document.getElementById('displayDate').innerHTML = 'On date: ' + dateInput;
+
+})
+
+function searchByCoords(latInput, longInput) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${longInput}&lon=${latInput}&appid=${appId}`).then(response => {
+        return response.json();
+    }).then(response => {
+        init(response);
+    })
+}
+
+
+
+
+var nextStep = document.getElementById('next-step');
+var coordsPage = document.getElementById('coords-page');
+var chooseValue = document.getElementById('choose-value');
+var cityIdZip = document.getElementById('cityIdZip');
+nextStep.addEventListener('click', onClick);
+function onClick() {
+    if (dropList.value === 'city' || dropList.value === 'id' || dropList.value === 'ZIP') {
+        chooseValue.classList.add('hide-show');
+        cityIdZip.classList.remove('hide-show');
+
+    }
+    else if
+        (dropList.value === 'coord') {
+        chooseValue.classList.add('hide-show');
+        coordsPage.classList.remove('hide-show');
+    }
+    
+
+    var dateInput = document.getElementById('dateInput').value;
+    console.log(dateInput);
+    var isoDate = new Date(dateInput).toISOString();
+    console.log(isoDate)
+
+    
+}
+
+console.log(dropList);
+function dropDwnList() {
+    displayText = dropList.options[dropList.selectedIndex].text;
+}
+const dateInput = document.getElementById('dateInput');
+    dateInput.valueAsNumber = Date.now()-(new Date()).getTimezoneOffset()*60000;
+
+
+
+
